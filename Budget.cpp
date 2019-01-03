03acd5b3a9a47bd252c2e4a652983f709541e9db
cesarmoo <cesarmu@umich.edu>
2019-01-02 22:14:35 -0500

    init

---
+++ Budget.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// this is the main parser of the program.
+// reads in the following user commands: 
+int main() {
+  string command;
+  cout << "Welcome. What would you like to do?\n";
+  
+}
